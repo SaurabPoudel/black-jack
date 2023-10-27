66999b8c1ea1204018d3fb2b8566868eb7b5ca24
SaurabPoudel <poudelsaurab20@gmail.com>
2023-10-27 22:43:30 +0545

    fix: header file correction

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,35 @@
-#include <SDL.h>
+#include <SDL2/SDL.h>
 
 int main() {
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-        return 1;
+  // Initialize SDL
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+    return 1;
+  }
+
+  // Create a window
+  SDL_Window *window =
+      SDL_CreateWindow("Blank Window", SDL_WINDOWPOS_UNDEFINED,
+                       SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+  if (!window) {
+    SDL_Log("Unable to create window: %s", SDL_GetError());
+    return 1;
+  }
+
+  // Main loop
+  bool quit = false;
+  while (!quit) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        quit = true;
+      }
     }
+  }
 
-    // Create a window
-    SDL_Window* window = SDL_CreateWindow("Blank Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
-    if (!window) {
-        SDL_Log("Unable to create window: %s", SDL_GetError());
-        return 1;
-    }
+  // Cleanup and quit
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 
-    // Main loop
-    bool quit = false;
-    while (!quit) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                quit = true;
-            }
-        }
-    }
-
-    // Cleanup and quit
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
-    return 0;
+  return 0;
 }
